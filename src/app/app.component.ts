793813a7c094ed2a2f3dafac458443cc853a9ef0
Luiz Vieira <luiz22_sp@hotmail.com>
2021-12-29 18:54:58 -0300

    Primeira parte do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   photos = [
-    { 
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg', 
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
       description: 'Leão'
     },
-    { 
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg', 
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
       description: 'Leoa'
     },
-    { 
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg', 
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
       description: 'Leoa'
-    }         
-  ];  
+    }
+  ];
 
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { PhotoComponent } from "./photo/photo.component";
 
 @NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ] 
+  declarations: [PhotoComponent],
+  exports: [PhotoComponent]
 })
-export class PhotosModule {}
+export class PhotosModule { }
