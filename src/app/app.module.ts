734f720f57a5375fa5b8f3990be65582f94b47f6
Luiz Vieira <luiz22_sp@hotmail.com>
2021-12-29 21:08:37 -0300

    03 integração com Web APIs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
 import { PhotosModule } from './photos/photos.module';
+
 
 @NgModule({
   declarations: [
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,9 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from "@angular/core";
 
 import { PhotoComponent } from "./photo/photo.component";
 
 @NgModule({
   declarations: [PhotoComponent],
+  imports: [HttpClientModule],
   exports: [PhotoComponent]
 })
 export class PhotosModule { }
