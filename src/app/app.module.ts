c136b8971f234119358877371200c1ea89faea4e
Luiz Vieira <luiz22_sp@hotmail.com>
2021-12-31 17:34:39 -0300

    04 Single Page Application e Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 import { PhotosModule } from './photos/photos.module';
 
 
@@ -10,7 +12,9 @@ import { PhotosModule } from './photos/photos.module';
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    AppRoutingModule,
+    PhotosModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,20 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from "./photo/photo.component";
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-  declarations: [PhotoComponent],
-  imports: [HttpClientModule],
-  exports: [PhotoComponent]
+  declarations: [
+    PhotoComponent, 
+    PhotoListComponent, 
+    PhotoFormComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ]
 })
 export class PhotosModule { }
